5b58148548f4222e3e62489415cc9dbc57814d1b
Emiliano Cabaña <emicaba@gmail.com>
2021-11-11 16:04:21 -0300

    PRIMER COMMIT

---
+++ script.js
@@ -0,0 +1,13 @@
+function Usuario(){
+
+let nombre = prompt("ingrese el nombre");
+let apellido = prompt("ingrese el apellido");
+let usuario = nombre + apellido;
+let contraseña = usuario;
+if(nombre!="" && apellido!=""){
+    alert ("Su usuario es:" + " " + usuario + "\n Su contrasena es:" + " " + contraseña + "\n PODRA MODIFICARLA EN SETTINGS");
+}else{
+    alert("Su nombre y apellido no pueden estar vacios"); Usuario()
+}}
+Usuario()
+;
