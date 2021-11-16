e19acc4da7e4dd86a3c62f9cac4ad93523aa7bf1
Emiliano Cabaña <emicaba@gmail.com>
2021-11-16 10:44:23 -0300

    SEGUNDO COMMIT

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
+class Usuario{
+    constructor (nombre, apellido, usuario, contraseña){
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.usuario = nombre+apellido;
+        this.contraseña = nombre+apellido;
+    }
+
+}
 function crearUsuario(){
 
-let nombre = prompt("ingrese el nombre");
-let apellido = prompt("ingrese el apellido");
-let usuario = nombre + apellido;
-let contraseña = usuario;
-if(nombre!="" && apellido!=""){
-    alert ("Su usuario es:" + " " + usuario + "\n Su contrasena es:" + " " + contraseña + "\n PODRA MODIFICARLA EN SETTINGS");
-}else{
-    alert("Su nombre y apellido no pueden estar vacios"); crearUsuario()
-}}
-crearUsuario()
-;
+    if(usuario1.nombre!="" && usuario1.apellido!=""){
+        alert ("Su usuario es:" + " " + usuario1.usuario + "\n Su contrasena es:" + " " + usuario1.contraseña + "\n PODRA MODIFICARLA EN SETTINGS");
+    }else{
+        alert("Su nombre y apellido no pueden estar vacios \n PRESIONE F5")
+    }}
+const usuario1 = new Usuario (prompt("ingrese el nombre"), prompt("ingrese el apellido"));
+
+crearUsuario();
