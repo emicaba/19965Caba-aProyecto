e5e38c4d67f888604b5115bf764c0c38a578900a
Emiliano Cabaña <emicaba@gmail.com>
2021-11-17 11:38:20 -0300

    TERCER COMMIT

--- script.js
+++ script.js
@@ -8,12 +8,28 @@ class Usuario{
 
 }
 function crearUsuario(){
-
+    const usuario1 = new Usuario (prompt("ingrese el nombre"), prompt("ingrese el apellido"));
     if(usuario1.nombre!="" && usuario1.apellido!=""){
         alert ("Su usuario es:" + " " + usuario1.usuario + "\n Su contrasena es:" + " " + usuario1.contraseña + "\n PODRA MODIFICARLA EN SETTINGS");
     }else{
-        alert("Su nombre y apellido no pueden estar vacios \n PRESIONE F5")
+        alert("Su nombre y apellido no pueden estar vacios \n Ingrese su nombre y apellido nuevamente"); crearUsuario()
     }}
-const usuario1 = new Usuario (prompt("ingrese el nombre"), prompt("ingrese el apellido"));
+
 
 crearUsuario();
+const rosasRed = ["Explorer", "Freedom"];
+const rosasHotPink = ["Pink Floyd", "Lola", "Hot Explorer", "Topaz", "Gotcha"];
+const rosasLavander = ["Cool Water", "Moody Blues"];
+const rosasPink = ["Esperance", "Frutetto", "Pink Mondial", "Hermosa", "Sweet Unique"];
+const rosasWhite = ["Mondial", "Playa Blanca", "Vendela", "Polarstar", "Moonstone"];
+const rosasPeach = ["Shimmer", "Tiffany"];
+const rosasOrange = ["Free Spirit", "Nina", "Orange Crush"];
+const rosasYellow = ["Bikini", "Brighton", "Stardust"];
+const rosasBicolor = ["Sweetness"];
+const rosasTeracota = ["Kahala"];
+const rosas = rosasRed.concat(rosasHotPink, rosasLavander, rosasPink, rosasWhite, rosasPeach, rosasOrange, rosasYellow, rosasBicolor, rosasTeracota);
+if(rosas.length === rosasRed.length + rosasHotPink.length + rosasLavander.length + rosasPink.length + rosasWhite.length + rosasPeach.length + rosasOrange.length + rosasYellow.length + rosasBicolor.length + rosasTeracota.length){
+console.log(rosas);
+} else {
+    console.log("Error de concatenacion");
+}
