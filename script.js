c5959debfbe92744eb269334dd8ae35cad810c7a
Emiliano Cabaña <emicaba@gmail.com>
2021-12-01 13:21:23 -0300

    primer commit SEGUNDA ENTREGA PROYECTO FINAL

--- script.js
+++ script.js
@@ -1,32 +1,61 @@
-class Usuario{
-    constructor (nombre, apellido, usuario, contraseña){
-        this.nombre = nombre;
-        this.apellido = apellido;
-        this.usuario = nombre+apellido;
-        this.contraseña = nombre+apellido;
-    }
+// class Usuario{
+//     constructor (nombre, apellido, usuario, contraseña){
+//         this.nombre = nombre;
+//         this.apellido = apellido;
+//         this.usuario = nombre+apellido;
+//         this.contraseña = nombre+apellido;
+//     }
 
+// }
+let floreria = document.getElementById("floreria").value;
+let personas = document.getElementById("persona").value;
+let mails = document.getElementById("mail").value;
+let direcciones = document.getElementById("direccion").value;
+let telefonos = document.getElementById("telefono").value;
+
+function crearClientes(marcacion, floristeria, persona, mail, direccion, telefono) {
+        this.marcacion = marcacion;
+        this.floristeria = floristeria;
+        this.persona = persona;
+        this.mail = mail;
+        this.direccion = direccion;
+        this.telefono = telefono;
+        let marcaciones = document.getElementById("marcacion").value;
+
+let cliente1 = new crearClientes (marcaciones, floreria,personas, mails, direcciones, telefonos);
+return cliente1;
 }
 
-function crearUsuario(){
-    const usuario1 = new Usuario (prompt("ingrese el nombre"), prompt("ingrese el apellido"));
+console.log(cliente1)
+class Cajas{
+    constructor (tipo,tallos,bunches){
+        this.tipo = tipo;
+        this.tallos = tallos;
+        const caja1 = new Clientes (QB, 100,bunches)
+        const caja2 = new Clientes (HB, 200,bunches)
 
-    if(usuario1.nombre!="" && usuario1.apellido!="") {
-        alert ("Su usuario es:" + " " + usuario1.usuario + "\n Su contrasena es:" + " " + usuario1.contraseña + "\n PODRA MODIFICARLA EN SETTINGS");
-        return usuario1;
-    } else {
-        alert("Su nombre y apellido no pueden estar vacios \n Ingrese su nombre y apellido nuevamente");
-        crearUsuario();
     }
 }
 
-const usuario = crearUsuario();
-const usuarioLogIn = document.getElementById ("logInUsuario")
+// function crearUsuario(){
+//     const usuario1 = new Usuario (prompt("ingrese el nombre"), prompt("ingrese el apellido"));
 
-    let impresionUsuario = document.createElement("p");
-    impresionUsuario.classList.add("impresionUsuarios");
-    impresionUsuario.innerHTML = "HOLA " + usuario.usuario
-    usuarioLogIn.appendChild(impresionUsuario);
+//     if(usuario1.nombre!="" && usuario1.apellido!="") {
+//         alert ("Su usuario es:" + " " + usuario1.usuario + "\n Su contrasena es:" + " " + usuario1.contraseña + "\n PODRA MODIFICARLA EN SETTINGS");
+//         return usuario1;
+//     } else {
+//         alert("Su nombre y apellido no pueden estar vacios \n Ingrese su nombre y apellido nuevamente");
+//         crearUsuario();
+//     }
+// }
+
+// const usuario = crearUsuario();
+// const usuarioLogIn = document.getElementById ("logInUsuario")
+
+//     let impresionUsuario = document.createElement("p");
+//     impresionUsuario.classList.add("impresionUsuarios");
+//     impresionUsuario.innerHTML = "HOLA " + usuario.usuario
+//     usuarioLogIn.appendChild(impresionUsuario);
 
 const rosasRed = ["Explorer", "Freedom"];
 const rosasHotPink = ["Pink Floyd", "Lola", "Hot Explorer", "Topaz", "Gotcha"];
